0ef95970f53fe724a3448354116c7625824781ae
Nicolas <nicolaspanico2002@gmail.com>
2025-10-21 18:25:37 -0300

    Agregar SSL para conexión PostgreSQL en Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { EtapasModule } from './etapas/etapas.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port:  parseInt(process.env.DB_PORT || '5432', 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
+
+      //  Render requiere SSL, pero mantenemos compatibilidad local
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
     }),
     AuthModule,
     UsuariosModule,
