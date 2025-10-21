f6ae830158fdf60ff128e57b8d962650dc8783b1
Nicolas <nicolaspanico2002@gmail.com>
2025-10-21 17:36:41 -0300

    añadido swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Colaboraciones')
+    .setDescription('Documentación de la API para proyectos y etapas')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // CORS habilitado por si lo necesitás
+  app.enableCors();
+
+  await app.listen(3000);
+  console.log('🚀 API corriendo en http://localhost:3000');
+  console.log('📘 Swagger: http://localhost:3000/api-docs');
 }
 bootstrap();
